22ef6a44c13bb78d690e5d4453efc6d2e696cc50
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-06-21 20:48:37 -0300

    chore: add graphql config

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
+import { ApolloProvider } from '@apollo/client';
 import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
+import { client } from './lib/apollo';
 
 import './styles/global.css';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
 );
