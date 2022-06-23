60496ac1b61cb0de7f4c7896530662976c93b5e2
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-06-22 21:01:02 -0300

    chore: feach lesson details

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import format from 'date-fns/format';
 import isPast from 'date-fns/isPast';
 import ptBR from 'date-fns/locale/pt-BR';
@@ -28,10 +29,10 @@ export function Lesson(props: LessonProps) {
   );
 
   return (
-    <a href={slug}>
+    <Link to={`/event/lesson/${slug}`} className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
@@ -55,6 +56,6 @@ export function Lesson(props: LessonProps) {
 
         <strong className="text-gray-200 mt-5 block">{title}</strong>
       </div>
-    </a>
+    </Link>
   );
 }
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -5,16 +5,62 @@ import {
   FileArrowDown,
   Lightning,
 } from 'phosphor-react';
-
+import { gql, useQuery } from '@apollo/client';
 import '@vime/core/themes/default.css';
 
-export function Video() {
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug($slug: String) {
+    lesson(where: { slug: $slug }) {
+      title
+      videoId
+      description
+      teacher {
+        bio
+        avatarURL
+        name
+      }
+    }
+  }
+`;
+
+type GetLessonBySlugResponse = {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      bio: string;
+      avatarURL: string;
+      name: string;
+    };
+  };
+};
+
+type VideoProps = {
+  lessonSlug: string;
+};
+
+export function Video(props: VideoProps) {
+  const { lessonSlug } = props;
+
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: { slug: lessonSlug },
+  });
+
+  if (!data) {
+    return (
+      <div className="flex-1">
+        <p>Carregando...</p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h=[60vh] aspect-video">
           <Player>
-            <Youtube videoId="SO4-izct7Mc" />
+            <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
           </Player>
         </div>
@@ -23,26 +69,25 @@ export function Video() {
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
           <div className="flex-1">
-            <h1 className="text-2xl font-bold">Aula 01</h1>
+            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
 
             <p className="mt-4 text-gray-200 leading-relaxed">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat
-              iste doloribus omnis aspernatur asperiores
+              {data.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 mt-6">
               <img
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src="https://github.com/luiz21goncalves.png"
-                alt="Avatar de Luiz Gonçalves"
+                src={data.lesson.teacher.avatarURL}
+                alt={`Avatar de ${data.lesson.teacher.name}`}
               />
 
               <div className="leading-relaxed">
                 <strong className="font-bold text-2xl block">
-                  Luiz Gonçalves
+                  {data.lesson.teacher.name}
                 </strong>
                 <span className="text-gray-200 text-sm block">
-                  Front end Developer @Ferreri
+                  {data.lesson.teacher.bio}
                 </span>
               </div>
             </div>
