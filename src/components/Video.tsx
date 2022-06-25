ae0e9b6235a840fb98959b7fff5ed1470065387e
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-06-25 11:01:00 -0300

    chore: add graphql-codegen

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -5,36 +5,10 @@ import {
   FileArrowDown,
   Lightning,
 } from 'phosphor-react';
-import { gql, useQuery } from '@apollo/client';
+
+import { useGetLessonBySlugQuery } from '../graphql/generated';
+
 import '@vime/core/themes/default.css';
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug($slug: String!) {
-    lesson(where: { slug: $slug }) {
-      title
-      videoId
-      description
-      teacher {
-        bio
-        avatarURL
-        name
-      }
-    }
-  }
-`;
-
-type GetLessonBySlugResponse = {
-  lesson: {
-    title: string;
-    videoId: string;
-    description: string;
-    teacher: {
-      bio: string;
-      avatarURL: string;
-      name: string;
-    };
-  };
-};
 
 type VideoProps = {
   lessonSlug: string;
@@ -43,11 +17,9 @@ type VideoProps = {
 export function Video(props: VideoProps) {
   const { lessonSlug } = props;
 
-  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
-    variables: { slug: lessonSlug },
-  });
+  const { data } = useGetLessonBySlugQuery({ variables: { slug: lessonSlug } });
 
-  if (!data) {
+  if (!data || !data.lesson) {
     return (
       <div className="flex-1">
         <p>Carregando...</p>
@@ -75,22 +47,24 @@ export function Video(props: VideoProps) {
               {data.lesson.description}
             </p>
 
-            <div className="flex items-center gap-4 mt-6">
-              <img
-                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src={data.lesson.teacher.avatarURL}
-                alt={`Avatar de ${data.lesson.teacher.name}`}
-              />
+            {data.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6">
+                <img
+                  className="h-16 w-16 rounded-full border-2 border-blue-500"
+                  src={data.lesson.teacher.avatarURL}
+                  alt={`Avatar de ${data.lesson.teacher.name}`}
+                />
 
-              <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">
-                  {data.lesson.teacher.name}
-                </strong>
-                <span className="text-gray-200 text-sm block">
-                  {data.lesson.teacher.bio}
-                </span>
+                <div className="leading-relaxed">
+                  <strong className="font-bold text-2xl block">
+                    {data.lesson.teacher.name}
+                  </strong>
+                  <span className="text-gray-200 text-sm block">
+                    {data.lesson.teacher.bio}
+                  </span>
+                </div>
               </div>
-            </div>
+            )}
           </div>
 
           <div className="flex flex-col gap-4">
