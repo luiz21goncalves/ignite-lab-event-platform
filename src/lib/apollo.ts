dd24cf31c6ff47ae5cf794c293846a51c773bbbb
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-06-25 09:41:17 -0300

    feat: add subscribe page

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,5 +2,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
   uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
